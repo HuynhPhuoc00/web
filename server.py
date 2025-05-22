79c31cd0a333adb31adb351f43d07e325b55ca93
Huỳnh Phước Lê <lehuynhphuoc19119121@gmail.com>
2025-05-22 21:23:38 +0700

    fix port

--- server.py
+++ server.py
@@ -2,8 +2,6 @@ from flask import Flask, request, jsonify, send_from_directory
 
 app = Flask(__name__, static_url_path='', static_folder='static')
 
-bulb_status = {"BULB_01": "OFF"}
-
 @app.route("/bulb", methods=["GET", "POST"])
 
 @app.route("/")
@@ -11,4 +9,4 @@ def root():
     return app.send_static_file("index.html")
 
 if __name__ == "__main__":
-    app.run(port=5000)
+    app.run(port=8080)
