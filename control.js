25220ad784ba8892b353e1995bfbd8ac8c470518
Huỳnh Phước Lê <lehuynhphuoc19119121@gmail.com>
2025-05-22 16:08:26 +0700

    fix btn for room

--- control.js
+++ control.js
@@ -1,3 +1,4 @@
+// Light
 var btn1 = document.getElementById("example_01");
 btn1.onclick = function(){
   if (document.getElementById("example_01").checked){
@@ -13,6 +14,7 @@ btn1.onclick = function(){
   }
 }
 
+// Fan
 var btn2 = document.getElementById("example_02");
 btn2.onclick = function(){
   if (document.getElementById("example_02").checked){
@@ -27,34 +29,37 @@ btn2.onclick = function(){
   }
 }
 
+// TV
 var btn3 = document.getElementById("example_03");
 btn3.onclick = function(){
   if (document.getElementById("example_03").checked){
-    document.getElementById("d03_img").src = "./img/kitchenon.png"  
+    document.getElementById("d03_img").src = "./img/tivion.png"  
     firebase.database().ref("/TT_IoT").update({
       "BULB_01": "ON"
     })
   } else {
-    document.getElementById("d03_img").src = "./img/kitchenoff.png"
+    document.getElementById("d03_img").src = "./img/tivioff.png"
     firebase.database().ref("/TT_IoT").update({
     })
   }
 }
 
+// Lock
 var btn4 = document.getElementById("example_04");
 btn4.onclick = function(){
   if (document.getElementById("example_04").checked){
-    document.getElementById("d04_img").src = "./img/ovenon.png"  
+    document.getElementById("d04_img").src = "./img/lock.png"  
     firebase.database().ref("/TT_IoT").update({
       "BULB_01": "ON"
     })
   } else {
-    document.getElementById("d04_img").src = "./img/ovenoff.png"
+    document.getElementById("d04_img").src = "./img/unlocked.png"
     firebase.database().ref("/TT_IoT").update({
     })
   }
 }
 
+// Air
 var btn5 = document.getElementById("example_05");
 btn5.onclick = function(){
   if (document.getElementById("example_05").checked){
