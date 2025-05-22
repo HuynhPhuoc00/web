dbe4c48d97fffcd4f5f4f298c63541a9a10cb9d8
Huỳnh Phước Lê <lehuynhphuoc19119121@gmail.com>
2025-05-22 17:10:06 +0700

    fix error

--- cpp_control.cpp
+++ cpp_control.cpp
@@ -1,11 +1,9 @@
 #include <cstdlib>
 
 int main() {
-    // Bật đèn
-    system("curl -X POST http://localhost:5000/bulb -H "Content-Type: application/json" -d '{"BULB_01": "ON"}'");
-    
-    // Tắt đèn
-    // system("curl -X POST http://localhost:5000/bulb -H \"Content-Type: application/json\" -d '{\"BULB_01\": \"OFF\"}'");
+    // turn on the bulb
+    system("curl -X POST http://localhost:5000/bulb -H \"Content-Type: application/json\" -d '{\"BULB_01\": \"ON\"}'");
+
 
     return 0;
 }
