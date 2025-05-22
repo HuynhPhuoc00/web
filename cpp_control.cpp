bda4b63172cbae3f4a1e80b8b710efddd72f8321
Huỳnh Phước Lê <lehuynhphuoc19119121@gmail.com>
2025-05-23 00:25:13 +0700

    Fix bug run 1 time

--- cpp_control.cpp
+++ cpp_control.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <curl/curl.h>
 #include <string>
-#include <nlohmann/json.hpp> 
+#include <thread>
+#include <chrono>
+#include <nlohmann/json.hpp>
 
 using json = nlohmann::json;
 
@@ -17,33 +19,46 @@ int main() {
 
     CURL* curl = curl_easy_init();
     if (!curl) {
-        std::cerr << "ERROR: Can't initialize CURL.\n";
+        std::cerr << "Error: unable to initialize CURL.\n";
         return 1;
     }
 
-    std::string response;
-    curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
-    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
-    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response);
+    std::string old_state = ""; // previous device state
 
-    CURLcode res = curl_easy_perform(curl);
-    if (res != CURLE_OK) {
-        std::cerr << "CURL FAILED: " << curl_easy_strerror(res) << "\n";
-        return 1;
-    }
+    while (true) {
+        std::string response;
+        curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response);
 
-    try {
-        json j = json::parse(response);
-        std::string state = j.at(device);
-        std::cout << "Device: " << device << " | State: " << state << "\n";
-
-        if (state == "ON") {
-            std::cout << "==> ON!" << std::endl;
+        CURLcode res = curl_easy_perform(curl);
+        if (res != CURLE_OK) {
+            std::cerr << "CURL failed: " << curl_easy_strerror(res) << "\n";
         } else {
-            std::cout << "==> OFF!" << std::endl;
+            try {
+                json j = json::parse(response);
+                std::string new_state = j.at(device);
+
+                // Only process if the state changed
+                if (new_state != old_state) {
+                    std::cout << "Device: " << device << " | New state: " << new_state << "\n";
+
+                    if (new_state == "ON") {
+                        std::cout << "==> TURN ON device!" << std::endl;
+                    } else {
+                        std::cout << "==> TURN OFF device!" << std::endl;
+                    }
+
+                    old_state = new_state;  // update old state
+                }
+                // If no change, do nothing
+
+            } catch (const std::exception& e) {
+                std::cerr << "JSON parsing error: " << e.what() << std::endl;
+            }
         }
-    } catch (const std::exception& e) {
-        std::cerr << "Error in analysis JSON: " << e.what() << std::endl;
+
+        std::this_thread::sleep_for(std::chrono::seconds(3));
     }
 
     curl_easy_cleanup(curl);
