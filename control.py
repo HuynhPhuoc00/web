53f216efa27432797ce4640f578e140152b2d684
Huỳnh Phước Lê <lehuynhphuoc19119121@gmail.com>
2025-05-22 21:27:29 +0700

    add file control

---
+++ control.py
@@ -0,0 +1,3 @@
+# control.py
+def update_led_state(state):
+    print(f"[control.py] Đèn được cập nhật: {state}")
--- server.py
+++ server.py
@@ -1,8 +1,8 @@
 from flask import Flask, request, jsonify
+import control 
 
 app = Flask(__name__, static_url_path='', static_folder='static')
 
-# Biến toàn cục để lưu trạng thái LED
 led_state = {"BULB_01": "OFF"}
 
 @app.route("/")
@@ -17,13 +17,14 @@ def bulb():
         data = request.get_json()
         if data and "BULB_01" in data:
             led_state["BULB_01"] = data["BULB_01"]
-            print(f"LED set to: {led_state['BULB_01']}")
+            
+            control.update_led_state(led_state["BULB_01"])
+
             return jsonify({"status": "ok", "LED": led_state["BULB_01"]})
         else:
             return jsonify({"error": "Missing BULB_01 key"}), 400
 
     elif request.method == "GET":
-        # Trả lại trạng thái hiện tại của LED
         return jsonify(led_state)
 
 if __name__ == "__main__":
