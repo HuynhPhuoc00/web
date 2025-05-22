4d7659d8817482c607c0ff742a211f0c4670a710
Huỳnh Phước Lê <lehuynhphuoc19119121@gmail.com>
2025-05-22 17:11:23 +0700

    change port in cpp file

--- cpp_control.cpp
+++ cpp_control.cpp
@@ -2,8 +2,6 @@
 
 int main() {
     // turn on the bulb
-    system("curl -X POST http://localhost:5000/bulb -H \"Content-Type: application/json\" -d '{\"BULB_01\": \"ON\"}'");
-
-
+    system("curl -X POST http://localhost:8080/bulb -H \"Content-Type: application/json\" -d '{\"BULB_01\": \"ON\"}'");
     return 0;
 }
